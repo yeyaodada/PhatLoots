558e7da975ebd982e5dfedb4fcfd4703beb76478
ShiftingKill <35326704+ShiftingKill@users.noreply.github.com>
2021-12-21 02:02:44 -0800

    Update POM, fixes small worldguard checking error
    Fixes bug as seen in https://pastebin.com/XFiGszw6 and in the discord bug reports. MythicMobs doesn't need to be an included JAR anymore, updated their Maven repo. WorldGuardRegionHook will abort if the world is null instead of trying to one-line call a giant method. Fixes some PhatLoots tasks that are still running trying to check on a world that has been deleted or something else.

--- src/com/codisimus/plugins/phatloots/regions/WorldGuardRegionHook.java
+++ src/com/codisimus/plugins/phatloots/regions/WorldGuardRegionHook.java
@@ -1,8 +1,10 @@
 package com.codisimus.plugins.phatloots.regions;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -24,7 +26,11 @@ public class WorldGuardRegionHook implements RegionHook {
     @Override
     public List<String> getRegionNames(Location loc) {
         List<String> regionNames = new ArrayList<>(1);
-        ApplicableRegionSet applicableRegionSet = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(loc.getWorld())).getApplicableRegions(BukkitAdapter.asBlockVector(loc));
+        org.bukkit.World bukkitWorld = loc.getWorld();
+        if (bukkitWorld == null)
+            return regionNames;
+        World world = BukkitAdapter.adapt(bukkitWorld);
+        ApplicableRegionSet applicableRegionSet = WorldGuard.getInstance().getPlatform().getRegionContainer().get(world).getApplicableRegions(BukkitAdapter.asBlockVector(loc));
         Set<ProtectedRegion> regionSet = applicableRegionSet.getRegions();
 
         //Eliminate all parent Regions
